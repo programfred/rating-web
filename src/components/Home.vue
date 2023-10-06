978d8ccbbea3cdaab827265323bd0fe7fb5b5b1e
programfred <programfred@gmail.com>
2023-10-06 11:25:18 +1100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="row at-row flex-center flex-middle">
       <div class="col-lg-24">
-        <h1 class="super-header">{{subtitle}}</h1>
+        <h1 class="super-header">{{subtitle}}-Fred Header</h1>
       </div>
     </div>
     <div class="row at-row flex-center flex-middle">
